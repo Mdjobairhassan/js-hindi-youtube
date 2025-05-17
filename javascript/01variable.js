7c4a997d0a8da7cee5bb7ddff9b2064e59b6b908
Mdjobairhassan <150878494+Mdjobairhassan@users.noreply.github.com>
2025-05-17 05:18:33 +0000

    data

--- javascript/01variable.js
+++ javascript/01variable.js
@@ -1,1 +1,17 @@
- console.log("Jobair")
+const AccountId = 125400251
+let AccountMAil = "Mdjobairhassan@#"
+var AccounttPassword = "25014"
+AccountCity = "Dhaka"
+var AccountStrat = "Jobair"
+
+
+// Account 02 // not Allowed
+
+AccountMAil = "nhhndtysafjdvedefty"
+AccounttPassword = "2580147"
+AccountCity = "Chittagam"
+
+console.log(AccountId);
+
+
+console.table([AccountId,AccountMAil,AccounttPassword,AccountCity,AccountStrat]);
---
+++ javascript/02Datatype.js
@@ -0,0 +1,10 @@
+console.log(3+3)
+
+console.log("Jobair");
+
+const Age = 54
+hight = "5"
+Weight = "Jobair"
+
+console.log(typeof Weight);
+
